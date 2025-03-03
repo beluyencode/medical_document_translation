6d443f8c52d1b03b60cc73899ed470d6be05f45c
beluyencode <longlc956@gmail.com>
2025-03-03 10:56:36 +0700

    1

--- document_service/client/src/app/directives/highlight-note-v2/highlight-note-v2.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightNoteV2Directive } from './highlight-note-v2.directive';
-
-describe('HighlightNoteV2Directive', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightNoteV2Directive();
-    expect(directive).toBeTruthy();
-  });
-});
--- document_service/client/src/environments/environment.dev.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    convertService: 'http://localhost:3000/',
-    onlyOffice: "http://localhost:9000/",
-    publicPath: "http://host.docker.internal:3000/",
-};
